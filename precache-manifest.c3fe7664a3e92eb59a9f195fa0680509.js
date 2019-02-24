5e36d403583349067c4967a07eabed914da49796
Andrei Savelyeu <raikaat@mail.ru>
2019-02-24 13:50:12 +0300

    Updates

---
+++ precache-manifest.c3fe7664a3e92eb59a9f195fa0680509.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5357f7305c488abea30c",
+    "url": "/mentor-dashboard/static/js/runtime~main.5357f730.js"
+  },
+  {
+    "revision": "427c5deee5b3956d2609",
+    "url": "/mentor-dashboard/static/js/main.427c5dee.chunk.js"
+  },
+  {
+    "revision": "0fcaddf638b91afb0c69",
+    "url": "/mentor-dashboard/static/js/2.0fcaddf6.chunk.js"
+  },
+  {
+    "revision": "427c5deee5b3956d2609",
+    "url": "/mentor-dashboard/static/css/main.2fcf7167.chunk.css"
+  },
+  {
+    "revision": "ff613f2f659f065d55b16747c4d7a4f9",
+    "url": "/mentor-dashboard/index.html"
+  }
+];
